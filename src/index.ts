9ff4b7006174e6d0b3ea86dd75bc77fa21e57fe0
Heyward Fann <fannheyward@gmail.com>
2020-09-25 14:41:21 +0800

    feat: add rome.bin configuration
    close #18

--- src/index.ts
+++ src/index.ts
@@ -1,90 +1,103 @@
 import {exec} from "child_process";
 import {
-  ExtensionContext,
-  LanguageClient,
-  LanguageClientOptions,
-  ServerOptions,
-  TransportKind,
-  commands,
-  services,
-  workspace,
+	ExtensionContext,
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+	TransportKind,
+	commands,
+	services,
+	workspace,
 } from "coc.nvim";
 import {existsSync} from "fs";
 import {join} from "path";
 import {promisify} from "util";
+import which from "which";
 
-const bin = join(
-  workspace.root,
-  "node_modules",
-  "rome",
-  "bin",
-  "rome",
-  "index.js",
-);
+function resolveRomeBin(): string {
+	// 1. rome.bin in coc-settings
+	// 2. $PATH
+	// 3. local node_modules
+	const cfg = workspace.getConfiguration("rome");
+	let bin = cfg.get<string>("bin");
+	if (bin && existsSync(bin)) {
+		return bin;
+	}
+
+	bin =
+		which.sync("rome", {nothrow: true}) ||
+		join(workspace.root, "node_modules", "rome", "bin", "rome", "index.js");
+	if (bin && existsSync(bin)) {
+		return bin;
+	}
+
+	return "";
+}
 
 async function romeInit(): Promise<void> {
-  const prompt = await workspace.showPrompt(
-    `Rome is found in your project, but configuration is missing, would you like to do 'rome init'?`,
-  );
-  if (!prompt) {
-    return;
-  }
+	const prompt = await workspace.showPrompt(
+		`Rome is found in your project, but configuration is missing, would you like to do 'rome init'?`,
+	);
+	if (!prompt) {
+		return;
+	}
 
-  const terminal = await workspace.createTerminal({cwd: workspace.root});
-  terminal.sendText(`${bin} init`);
-  await terminal.show(true);
+	const bin = resolveRomeBin();
+	const terminal = await workspace.createTerminal({cwd: workspace.root});
+	terminal.sendText(`${bin} init`);
+	await terminal.show(true);
 }
 
 export async function activate(context: ExtensionContext): Promise<void> {
-  const cfg = workspace.getConfiguration("rome");
-  const enable = (cfg.get("enable") as boolean);
-  if (!enable) {
-    return;
-  }
+	const cfg = workspace.getConfiguration("rome");
+	const enable = (cfg.get("enable") as boolean);
+	if (!enable) {
+		return;
+	}
 
-  if (!existsSync(bin)) {
-    workspace.showMessage("No rome found in your project root.", "warning");
-    return;
-  }
+	const bin = resolveRomeBin();
+	if (!bin) {
+		return;
+	}
 
-  const rjson = join(workspace.root, ".config", "rome.rjson");
-  if (!existsSync(rjson)) {
-    await romeInit();
-  }
+	const rjson = join(workspace.root, ".config", "rome.rjson");
+	if (!existsSync(rjson)) {
+		await romeInit();
+	}
 
-  const serverOptions: ServerOptions = {
-    module: bin,
-    args: ["lsp"],
-    transport: TransportKind.stdio,
-  };
+	const serverOptions: ServerOptions = {
+		module: bin,
+		args: ["lsp"],
+		transport: TransportKind.stdio,
+	};
 
-  const clientOptions: LanguageClientOptions = {
-    outputChannel: workspace.createOutputChannel("Rome"),
-    progressOnInitialization: true,
-    documentSelector: [
-      {scheme: "file", language: "javascript"},
-      {scheme: "file", language: "javascriptreact"},
-      {scheme: "file", language: "typescript"},
-      {scheme: "file", language: "typescriptreact"},
-      {scheme: "file", language: "json"},
-    ],
-  };
+	const clientOptions: LanguageClientOptions = {
+		outputChannel: workspace.createOutputChannel("Rome"),
+		progressOnInitialization: true,
+		documentSelector: [
+			{scheme: "file", language: "javascript"},
+			{scheme: "file", language: "javascriptreact"},
+			{scheme: "file", language: "typescript"},
+			{scheme: "file", language: "typescriptreact"},
+			{scheme: "file", language: "json"},
+		],
+	};
 
-  const client = new LanguageClient(
-    "rome",
-    "Rome",
-    serverOptions,
-    clientOptions,
-  );
-  context.subscriptions.push(services.registLanguageClient(client));
+	const client = new LanguageClient(
+		"rome",
+		"Rome",
+		serverOptions,
+		clientOptions,
+	);
+	context.subscriptions.push(services.registLanguageClient(client));
 
-  context.subscriptions.push(
-    commands.registerCommand(
-      "rome.cacheClear",
-      async () => {
-        const {stdout} = await promisify(exec)(`${bin} cache clear`);
-        workspace.showMessage(stdout);
-      },
-    ),
-  );
+	context.subscriptions.push(
+		commands.registerCommand(
+			"rome.cacheClear",
+			async () => {
+				const {stdout} = await promisify(exec)(`${bin} cache clear`);
+				workspace.showMessage(stdout);
+			},
+		),
+	);
 }
