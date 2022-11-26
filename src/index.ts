fcb35eba3b86a67989b95a0cf8a0904e2b3232c0
Heyward Fann <fannheyward@gmail.com>
2022-11-26 19:07:29 +0800

    feat!: rome next

--- src/index.ts
+++ src/index.ts
@@ -1,95 +1,96 @@
-import {exec} from "child_process";
 import {
 	ExtensionContext,
 	LanguageClient,
 	LanguageClientOptions,
 	ServerOptions,
-	TransportKind,
-	commands,
 	services,
-	window,
 	workspace,
 } from "coc.nvim";
-import {existsSync} from "fs";
-import {join} from "path";
-import {promisify} from "util";
+import { existsSync } from "fs";
+import { join } from "path";
+
+type Architecture = "x64" | "arm64";
+type PlatformTriplets = {
+	[P in NodeJS.Platform]?: {
+		[A in Architecture]: {
+			triplet: string;
+			package: string;
+		};
+	};
+};
+
+const PLATFORMS: PlatformTriplets = {
+	win32: {
+		x64: {
+			triplet: "x86_64-pc-windows-msvc",
+			package: "@rometools/cli-win32-x64/rome.exe",
+		},
+		arm64: {
+			triplet: "aarch64-pc-windows-msvc",
+			package: "@rometools/cli-win32-arm64/rome.exe",
+		},
+	},
+	darwin: {
+		x64: {
+			triplet: "x86_64-apple-darwin",
+			package: "@rometools/cli-darwin-x64/rome",
+		},
+		arm64: {
+			triplet: "aarch64-apple-darwin",
+			package: "@rometools/cli-darwin-arm64/rome",
+		},
+	},
+	linux: {
+		x64: {
+			triplet: "x86_64-unknown-linux-gnu",
+			package: "@rometools/cli-linux-x64/rome",
+		},
+		arm64: {
+			triplet: "aarch64-unknown-linux-gnu",
+			package: "@rometools/cli-linux-arm64/rome",
+		},
+	},
+};
 
 function resolveRomeBin(): string {
 	// 1. rome.bin in coc-settings
 	// 2. local node_modules
 	const cfg = workspace.getConfiguration("rome");
-	let bin = cfg.get<string>("bin");
+	let bin = cfg.get<string | null>("bin", null);
 	if (bin && existsSync(bin)) {
 		return bin;
 	}
 
-	bin = join(workspace.root, "node_modules", "rome", "bin", "rome", "index.js");
-	return existsSync(bin) ? bin : "";
-}
-
-async function romeInit(): Promise<void> {
-	const prompt = await window.showPrompt(
-		`Rome is found in your project, but configuration is missing, would you like to do 'rome init'?`,
-	);
-	if (!prompt) {
-		return;
+	bin = join(workspace.root, "node_modules", "rome", "bin", "rome");
+	if (existsSync(bin)) {
+		const packageName = PLATFORMS[process.platform]?.[process.arch]?.package;
+		return join(workspace.root, "node_modules", packageName);
 	}
-
-	const bin = resolveRomeBin();
-	const terminal = await workspace.createTerminal({cwd: workspace.root});
-	terminal.sendText(`${bin} init`);
-	await terminal.show(true);
+	return "";
 }
 
 export async function activate(context: ExtensionContext): Promise<void> {
-	const cfg = workspace.getConfiguration("rome");
-	const enable = (cfg.get("enable") as boolean);
+	const enable = workspace.getConfiguration("rome").get<boolean>("enable");
 	if (!enable) {
 		return;
 	}
 
-	const bin = resolveRomeBin();
-	if (!bin) {
+	const command = resolveRomeBin();
+	if (!command) {
 		return;
 	}
 
-	const rjson = join(workspace.root, ".config", "rome.rjson");
-	if (!existsSync(rjson)) {
-		await romeInit();
-	}
-
-	const serverOptions: ServerOptions = {
-		module: bin,
-		args: ["lsp"],
-		transport: TransportKind.stdio,
-	};
-
+	const serverOptions: ServerOptions = { command, args: ["lsp-proxy"] };
 	const clientOptions: LanguageClientOptions = {
 		progressOnInitialization: true,
 		documentSelector: [
-			{scheme: "file", language: "javascript"},
-			{scheme: "file", language: "javascriptreact"},
-			{scheme: "file", language: "typescript"},
-			{scheme: "file", language: "typescriptreact"},
-			{scheme: "file", language: "json"},
+			{ scheme: "file", language: "javascript" },
+			{ scheme: "file", language: "javascriptreact" },
+			{ scheme: "file", language: "typescript" },
+			{ scheme: "file", language: "typescriptreact" },
 		],
 	};
 
-	const client = new LanguageClient(
-		"rome",
-		"Rome",
-		serverOptions,
-		clientOptions,
-	);
+	const client = new LanguageClient("rome", serverOptions, clientOptions);
 	context.subscriptions.push(services.registLanguageClient(client));
-
-	context.subscriptions.push(
-		commands.registerCommand(
-			"rome.cacheClear",
-			async () => {
-				const {stdout} = await promisify(exec)(`${bin} cache clear`);
-				window.showMessage(stdout);
-			},
-		),
-	);
 }
